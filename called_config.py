0d4503d9663bdab9adf432d8ff887263dc9e800d
yangchandle <xuchandle@gmail.com>
2017-05-29 23:03:48 +0800

    Add SQLAlchemy extension, add class Task , change the db_created.py
    FLaskTaskr part2 DATABASE Mangement

--- called_config.py
+++ called_config.py
@@ -7,9 +7,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'myprecious'
+CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
 
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- db_create.py
+++ db_create.py
@@ -1,29 +1,18 @@
 # FlaskTaskr/db_create.py
 
 
-import sqlite3
-from called_config import DATABASE_PATH
+from views import db
+from models import Task 
+from datetime import date 
+
+# create the database and the db table
+db.create_all()
 
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+# insert data
+db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, 1))
 
 
-	# get a cursor object used to execute SQL commands
-	c = connection.cursor()
-
-
-	# create the table
-	c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-		status INTEGER NOT NULL)""")
-
-
-	# insert dummy data into the table
-	c.execute(
-		'INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
-	)
-	c.execute(
-		'INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-		)
+# commit the changes
+db.session.commit()
