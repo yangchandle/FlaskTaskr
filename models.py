327024fefa4d4dcc3fc7a7a01d61693b0390e8f7
yangchandle <xuchandle@gmail.com>
2017-05-30 11:10:58 +0800

    finish the flasktask part2
    add DATABASE Management, User Registration, User Login/Authentication, Database Relationships, Managing Sessions

--- models.py
+++ models.py
@@ -4,6 +4,9 @@
 from views import db
 
 
+import datetime
+
+
 class Task(db.Model):
 
 
@@ -14,14 +17,18 @@ class Task(db.Model):
 	name = db.Column(db.String, nullable=False)
 	due_date = db.Column(db.Date, nullable=False)
 	priority = db.Column(db.Integer, nullable=False)
+	posted_date = db.Column(db.Date, default=datetime.datetime.utcnow())
 	status = db.Column(db.Integer)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
-	def __init__(self, name, due_date, priority, status):
+	def __init__(self, name, due_date, priority, posted_date, status, user_id):
 		self.name = name
 		self.due_date = due_date
 		self.priority = priority
+		self.posted_date = posted_date
 		self.status = status
+		self.user_id = user_id
 
 	def __repr__(self):
 		return '<name {0}>'.format(self.name)
@@ -36,6 +43,7 @@ class User(db.Model):
 	name = db.Column(db.String, unique=True, nullable=False)
 	email = db.Column(db.String, unique=True, nullable=False)
 	password = db.Column(db.String, nullable=False)
+	tasks = db.relationship('Task', backref='poster')
 
 
 
--- views.py
+++ views.py
@@ -15,6 +15,7 @@ from forms import AddTaskForm, RegisterForm, LoginForm
 
 from flask_sqlalchemy import SQLAlchemy
 
+import datetime
 
 # config
 
@@ -48,19 +49,21 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('user_id', None)
 	flash('Goodbye')
 	return redirect(url_for('login'))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-	error =None
+	error = None
 	form = LoginForm(request.form)
 	if request.method == 'POST':
 		if form.validate_on_submit():
 			user = User.query.filter_by(name=request.form['name']).first()
 			if user is not None and user.password == request.form['password']:
 				session['logged_in'] = True
+				session['user_id'] = user.id
 				flash('Welcome! ')
 				return redirect(url_for('tasks'))
 			else:
@@ -97,12 +100,18 @@ def new_task():
 				form.name.data,
 				form.due_date.data,
 				form.priority.data,
-				'1'
+				datetime.datetime.utcnow(),
+				'1',
+				session['user_id']
 			)
 			db.session.add(new_task)
 			db.session.commit()
-		flash('New entry was successfully posted. Thanks.')
-	return redirect(url_for('tasks'))
+			flash('New entry was successfully posted. Thanks.')
+			return redirect(url_for('tasks'))
+		else:
+			flash('All fields are required.')
+			return redirect(url_for('tasks'))
+	return render_template('tasks.html', form=form)
 
 
 # Mark tasks as complete
