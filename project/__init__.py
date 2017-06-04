b958a398dd2d944f80a86ceb83dd556c76875197
yangchandle <xuchandle@gmail.com>
2017-06-04 22:52:55 +0800

    FlaskTaskr part 6 Password Hashing and custom error pages
    add password hashing and custom Error Pages

--- project/__init__.py
+++ project/__init__.py
@@ -1,12 +1,13 @@
 # project/__init__.py
 
-
-from flask import Flask
+import datetime
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy 
-
+from flask_bcrypt import Bcrypt 
 
 app = Flask(__name__)
 app.config.from_pyfile('_config.py')
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
@@ -16,3 +17,25 @@ from project.tasks.views import tasks_blueprint
 # register our blueprint
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+
+@app.errorhandler(404)
+def page_not_found(error):
+	if app.debug is not True:
+		now = datetime.datetime.now()
+		r = request.url 
+		with open('error.log', 'a') as f:
+			current_tiemstamp = now.strftime("%d-%m-%Y %H:%M:%S")
+			f.write("\n404 error at {}: {}".format(current_tiemstamp, r))
+	return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	if app.debug is not True:
+		now = datetime.datetime.now()
+		r = request.url 
+		with open('error.log', 'a') as f:
+			current_tiemstamp = now.strftime("%d-%m-%Y %H:%M:%S")
+			f.write("\n500 error at {}: {}".format(current_tiemstamp, r))
+	return render_template('500.html'), 500
--- test/test_users.py
+++ test/test_users.py
@@ -4,7 +4,7 @@
 import os
 import unittest
 
-from project import app, db
+from project import app, db, bcrypt
 from project._config import basedir
 from project.models import User
 
@@ -21,10 +21,14 @@ class UsersTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
+
+
+        self.assertEqual(app.debug, False)
 
     # executed after each test
     def tearDown(self):
@@ -51,7 +55,10 @@ class UsersTests(unittest.TestCase):
         return self.app.get('logout/', follow_redirects=True)
 
     def create_user(self, name, email, password):
-        new_user = User(name=name, email=email, password=password)
+        new_user = User(
+            name=name, 
+            email=email,
+            password=bcrypt.generate_password_hash(password))
         db.session.add(new_user)
         db.session.commit()
 
@@ -65,7 +72,7 @@ class UsersTests(unittest.TestCase):
         ), follow_redirects=True)
 
     def test_users_can_register(self):
-        new_user = User("michael", "michael@mherman.org", "michaelherman")
+        new_user = User("michael", "michael@mherman.org", bcrypt.generate_password_hash("michaelherman"))
         db.session.add(new_user)
         db.session.commit()
         test = db.session.query(User).all()
