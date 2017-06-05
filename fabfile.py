5ee94f3bb247ed0bc4107b147f4f65d9b05b7fc9
yangchandle <xuchandle@gmail.com>
2017-06-05 21:40:12 +0800

    add fabric module

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,16 @@
 
-from fabric.api import local
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
 import sys
 
 
+# prep
 def test():
-	local("nosetests -v")
+	with settings(warn_only=True):
+		result = local("nosetests -v", capture=True)
+	if result.failed and not confirm("Tests failed. Continue?"):
+		abort("Aborted at user request.")
+	
 
 
 def commit():
@@ -13,10 +19,26 @@ def commit():
 
 
 def push():
-	local("git push origin origin")
+	local("git push origin master")
 
+def heroku():
+	local("git push heroku master")
+
+def heroku_test():
+	local("heroku run nosetests -v")
+
+def deploy():
+	pull()
+	test()
+	commit()
+	heroku()
+	heroku_test()
 
 def prepare():
 	test()
 	commit()
 	push()
+
+
+def rollback():
+	local("heroku rollback")
